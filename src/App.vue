0fccfdfa11f85f39b65adcb64bf0732d958df86c
Flascher <alexanderflasch@gmail.com>
2017-09-26 12:59:36 -0500

    Initial setup

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<style lang="scss">
+    
+</style>
+
+
+<template>
+  
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
